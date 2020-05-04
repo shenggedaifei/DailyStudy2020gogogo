b8bc61747016eb376d1a147ba1ccfcd61c3f20ae
wangliangsheng <975135062@qq.com>
2020-05-05 00:23:19 +0800

    Array新方法

---
+++ src/wls/stu_algorithm/lineFindLastN.js
@@ -0,0 +1,26 @@
+var data = {
+	data: 1,
+	next:{
+		data:2,
+		next:{
+			data:8,
+			next:{
+				
+			}
+		}
+	}
+}
+
+function createLinkList(n){
+	var end = {
+				data: i,
+				next: null
+			}
+	for (var i=0; i< n; i++) {
+		var linkList = 
+	}
+	return linkList;
+}
+var List = createLinkList(3);
+console.log(List);
+
